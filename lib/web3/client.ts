31473da5dba2394e060bdcd5cfe2c4031a568a0d
lorraineS <lorraine.univ@gmail.com>
2024-01-09 11:12:06 +0100

    added connect's supported chains to default conf settings (#20)

--- lib/web3/client.ts
+++ lib/web3/client.ts
@@ -1,8 +1,42 @@
-import { polygon, polygonMumbai } from "@wagmi/core/chains"
+import {
+  arbitrum,
+  arbitrumNova,
+  avalanche,
+  base,
+  bsc,
+  celo,
+  cronos,
+  fantom,
+  gnosis,
+  mainnet,
+  moonbeam,
+  moonriver,
+  optimism,
+  polygon,
+  polygonMumbai,
+  polygonZkEvm,
+} from "@wagmi/chains"
 import { configureChains } from "wagmi"
 import { publicProvider } from "wagmi/providers/public"
 
 export const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [polygon, polygonMumbai],
+  [
+    polygon,
+    polygonMumbai,
+    gnosis,
+    avalanche,
+    mainnet,
+    bsc,
+    celo,
+    fantom,
+    moonbeam,
+    moonriver,
+    cronos,
+    optimism,
+    arbitrum,
+    arbitrumNova,
+    polygonZkEvm,
+    base,
+  ],
   [publicProvider()]
 )
