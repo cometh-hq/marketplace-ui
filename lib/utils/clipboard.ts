ae629c7c72a460601028a2833d96651eb18c319a
lorraines <lorraine.univ@gmail.com>
2024-02-29 11:02:24 +0100

    Fix duration for clipboard

--- lib/utils/clipboard.ts
+++ lib/utils/clipboard.ts
@@ -20,7 +20,7 @@ export const useClipboard = (): [CopiedValue, CopyFn] => {
       toast({
         title: "Copied!",
         description: text,
-        duration: 1000,
+        duration: 3000,
       })
       return true
     } catch (error) {
