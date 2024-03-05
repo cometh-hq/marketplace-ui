4e90252aa49041f841716b9399d641f8cd592349
lorraines <lorraine.univ@gmail.com>
2024-03-05 19:55:52 +0100

    Price route simplified

--- app/api/fiat-currency-price/route.ts
+++ app/api/fiat-currency-price/route.ts
@@ -4,28 +4,17 @@ export const revalidate = 60 * 10
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
-  const id = searchParams.get("id")
-  const currency = searchParams.get("currency")
+  const { id, currency } = Object.fromEntries(searchParams)
 
-  try {
-    if (!id || !currency) {
-      throw new Error("Invalid request")
-    }
-
-    const res = await coingeckoClient.get(
-      `/simple/price?ids=${id}&vs_currencies=${currency}`
-    )
-
-    const currentFiatPrice = res.data
-
-    return new Response(JSON.stringify({ currentFiatPrice }), {
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-  } catch (e: any) {
-    return new Response(e.message, {
-      status: 400,
-    })
+  if (!id || !currency) {
+    throw new Error("Invalid request")
   }
+
+  const res = await coingeckoClient.get(
+    `/simple/price?ids=${id}&vs_currencies=${currency}`
+  )
+
+  const currentFiatPrice = res.data
+
+  return Response.json({ currentFiatPrice }, { status: 200 })
 }
