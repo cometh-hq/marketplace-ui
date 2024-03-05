20fe903ed798c2856f6eb9777a2983be041d52dc
lorraines <lorraine.univ@gmail.com>
2024-03-05 20:03:11 +0100

    Add try / catch for GET request

--- app/api/fiat-currency-price/route.ts
+++ app/api/fiat-currency-price/route.ts
@@ -6,15 +6,19 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const { id, currency } = Object.fromEntries(searchParams)
 
-  if (!id || !currency) {
-    throw new Error("Invalid request")
+  try {
+    if (!id || !currency) {
+      throw new Error("Invalid request")
+    }
+
+    const res = await coingeckoClient.get(
+      `/simple/price?ids=${id}&vs_currencies=${currency}`
+    )
+
+    const currentFiatPrice = res.data
+
+    return Response.json({ currentFiatPrice }, { status: 200 })
+  } catch (error: any) {
+    return Response.json({ error: error.message }, { status: 500 })
   }
-
-  const res = await coingeckoClient.get(
-    `/simple/price?ids=${id}&vs_currencies=${currency}`
-  )
-
-  const currentFiatPrice = res.data
-
-  return Response.json({ currentFiatPrice }, { status: 200 })
 }
